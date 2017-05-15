d73ed814fcded3949a5912b94df0dd4a820864d0
seokjunbing <sjun.bing@gmail.com>
2017-05-15 18:28:26 -0400

    more rename

--- app/models/music_model.js
+++ app/models/music_model.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from 'mongoose';
 
 // mongoose.Promise = global.Promise;
 
-// create a PostSchema with a title field
+// create a musicSchema with a title field
 const MusicSchema = new Schema(
 
   {
@@ -17,7 +17,7 @@ const MusicSchema = new Schema(
 
 );
 
-// create PostModel class from schema
+// create musicModel class from schema
 const MusicModel = mongoose.model('Music', MusicSchema);
 
 export default MusicModel;
